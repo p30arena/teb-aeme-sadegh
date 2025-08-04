b1676f7627c6d1ae53533f1dedc51237aec17a5f
aliep <p30arena@gmail.com>
2025-08-04 07:15:43 +0300

    fix

--- translate.js
+++ translate.js
@@ -1,23 +1,24 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import fs from "fs/promises";
-import dotenv from "dotenv";
+const { GoogleGenAI } = require("@google/genai");
+const fs = require("fs/promises");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
+const ai = new GoogleGenAI({});
 
 async function translateText(text, isTitle = false) {
   const prefix = "Translate the following Arabic text to Farsi:";
-  const title_sufix = "Do not translate the word 'باب' and keep it as it is.";
+  const title_sufix = "The word 'باب' stands for 'موضوع'.";
   const prompt = `${prefix} ${isTitle ? title_sufix : ""}
 ---
 ${text}
 `;
   try {
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    return response.text();
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash-lite",
+        contents: prompt,
+    });
+    return response.text;
   } catch (error) {
     console.error("Error translating text:", error);
     // Return original text if translation fails
